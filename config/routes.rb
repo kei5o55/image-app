035a221aabdfee7fe05163b6ffb88bfbaa129d06
kei5o55 <uttk2003@gmail.com>
2026-07-30 14:57:39 +0900

    フロントとの疎通テスト用

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [ :index ]
+      get 'health', to: 'health#index'
     end
   end
 end
