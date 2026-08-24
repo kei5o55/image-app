4e455eb34ae260f2111746fad5f5ac24b2009c2a
kei5o55 <uttk2003@gmail.com>
2026-08-24 21:51:45 +0900

    tagsコントローラレスポンスの形式変更

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,11 +2,9 @@ module Api
   module V1
     class TagsController < ApplicationController
       def index
-        tags = Tag.order(:name)
+        tags = Tag.order(:name)        
 
-        render json: {
-          tags: tags
-        }, status: :ok
+        render json: tags, status: :ok
       end
     end
   end
